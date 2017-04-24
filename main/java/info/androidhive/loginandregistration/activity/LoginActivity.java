04f4244984729c373b84286f349b0fbbcafdf863
med360 <siya@azmediame.net>
2017-04-24 15:54:37 +0400

    Update LoginActivity.java

--- main/java/info/androidhive/loginandregistration/activity/LoginActivity.java
+++ main/java/info/androidhive/loginandregistration/activity/LoginActivity.java
@@ -42,7 +42,7 @@ public class LoginActivity extends Activity {
     private ProgressDialog pDialog;
     private SessionManager session;
     private SQLiteHandler db;
-    private String name2="";
+    private String name3="";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
